d47ea6629ca6ff876310c5adb3f2b078adc73503
Luis Fabiano <luissfabiano.dev@gmail.com>
2026-08-18 11:18:21 -0300

    feat: OrderStatus enum

--- src/main/java/com/luisdev/course/entities/Order.java
+++ src/main/java/com/luisdev/course/entities/Order.java
@@ -1,7 +1,7 @@
 package com.luisdev.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.luisdev.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -24,8 +24,26 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+        this.id = id;
+        this.moment = moment;
+        setOrderStatus(orderStatus);
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
 
 }
