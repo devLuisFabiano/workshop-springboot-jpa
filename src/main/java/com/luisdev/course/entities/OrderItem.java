8e35cfad191f151b010b10ce3892a4e5566ebf61
Luis Fabiano <luissfabiano.dev@gmail.com>
2026-09-15 12:04:26 -0300

    OrderItem part 1

---
+++ src/main/java/com/luisdev/course/entities/OrderItem.java
@@ -0,0 +1,55 @@
+package com.luisdev.course.entities;
+
+import com.luisdev.course.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+    @Setter
+    @Getter
+    private Integer quantity;
+    @Setter
+    @Getter
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product getProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+}
