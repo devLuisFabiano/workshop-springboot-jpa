74fc6abf16913e60d12c4f7a84055b61d28b67d4
Luis Fabiano <luissfabiano.dev@gmail.com>
2026-09-16 10:22:41 -0300

    Payment one-to-one association

---
+++ src/main/java/com/luisdev/course/entities/Payment.java
@@ -0,0 +1,35 @@
+package com.luisdev.course.entities;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_payments")
+public class Payment implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment(Long id, Order order, Instant moment) {
+        this.id = id;
+        this.order = order;
+        this.moment = moment;
+    }
+}
